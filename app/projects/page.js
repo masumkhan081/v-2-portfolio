7d822a69933dfd11c7bcc9f4519d6165f82f277c
masum khan <study.contents.of.masumkhan@gmail.com>
2023-05-22 18:01:47 +0600

    spring anim added

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,11 +1,24 @@
+"use client";
 import React from "react";
-
+import { animated, useSpring } from "@react-spring/web";
+//
 export default function page() {
+  const springs = useSpring({
+    from: { x: 0 },
+    to: { x: 100 },
+  });
+  //
   return (
-    <div className="border border-orange-800">
-      <span className="text-3xl block text-center border border-t-orange-300 w-full ">
+    <animated.div
+      style={{
+        borderRadius: "5px",
+        ...springs,
+      }}
+      className="w-1/2"
+    >
+      <span className="text-3xl block text-center border border-t-orange-300 h-full w-full ">
         Live Projects With Link To Be Here
       </span>
-    </div>
+    </animated.div>
   );
 }
