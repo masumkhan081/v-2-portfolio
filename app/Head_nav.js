d57c82f7946c4bdcc5b8068c2934c5734ac607b0
masum khan <study.contents.of.masumkhan@gmail.com>
2023-06-06 21:59:52 +0600

    pokath !

--- app/Head_nav.js
+++ app/Head_nav.js
@@ -68,7 +68,7 @@ export default function Head_nav() {
           className="rounded-s-md italic border-l-2 border-amber-500 sm:text-base text-xs sm:shadow-md shadow-sm shadow-teal-400 sm:w-fit w-full sm:ps-0 ps-5 text-orange-200 delay-100 hover:text-teal-200 hover:-translate-y-0.5 hover:scale-110 hover:rounded-xl hover:bg-teal-600 duration-200"
         >
           <a
-            href="./resources/resume/CV - Masum Khan.pdf"
+            href="./resources/resume/CV_mern-developer_masum-khan.pdf"
             download
             className="sm:p-1 px-1 p-0 "
           >
