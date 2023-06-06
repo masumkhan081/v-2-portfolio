a50d77ec152afc71df378440a7207191cb8c6429
masum khan <study.contents.of.masumkhan@gmail.com>
2023-06-06 22:22:35 +0600

    resume updated

--- app/skills/FrontEnd.js
+++ app/skills/FrontEnd.js
@@ -5,10 +5,10 @@ export default function FrontEnd() {
   return (
     <div className="bg-gradient-to-b from-teal-600 rounded-md bg-opacity-75 md:w-1/3 sm:w-1/2 w-screen p-3 bg-teal-800">
       <span className="block text-base font-bold bg-teal-800 text-green-200 text-center rounded-sm">
-        <SkillSetIcon clsnames="me-1 inline w-6 h-6"/> Front-End
+        <SkillSetIcon clsnames="me-1 inline w-6 h-6" /> Front-End
       </span>
       <ul className="my-3  space-y-2 text-green-200">
-        <li className="mb-1">
+        <li className="mb-2 ">
           <span className=" mr-1 rounded-md bg-teal-900 text-slate-300  px-1 ">
             CSS frameworks
           </span>
